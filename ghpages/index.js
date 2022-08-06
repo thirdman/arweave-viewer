d2450af72c506e6953f8f6cfd3dd74d50b64f887
Gareth Bedford <g@thirdman.co.nz>
2022-08-06 12:29:18 +0700

    make pages

---
+++ ghpages/index.js
@@ -0,0 +1,13 @@
+// // Pull in WebComponents polyfill for old, crappy browsers
+// import '@webcomponents/webcomponentsjs/webcomponents-loader';
+
+// // Import demo files:
+// import '../src/composited-card.component';
+// import './interactive-demo';
+// import './assets/styles/global.css';
+
+// Include global styles for the duplicate dom rendered composite-card
+// import { getStyles } from '../src/styles';
+// const styles = document.createElement('style');
+// styles.innerHTML = getStyles().cssText.replace(':host', '.compositedCardDomRender');
+// document.head.appendChild(styles);
